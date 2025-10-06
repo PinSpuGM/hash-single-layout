edb719cbbde276e5d868f70f486d210628d9b407
Pin <pin.ch@spu.ac.th>
2025-10-06 15:52:19 +0700

    edit paths with append /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hash-single-layout',
+  base: '/hash-single-layout/',
 })
